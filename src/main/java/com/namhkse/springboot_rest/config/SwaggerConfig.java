83a4f4880e32b35a31df9de77944a8fbed18f156
namhk <namhkse@gmail.com>
2024-07-29 05:17:59 +0700

    init

---
+++ src/main/java/com/namhkse/springboot_rest/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.namhkse.springboot_rest.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("This is a sample API"));
+    }
+}
---
+++ src/test/java/com/namhkse/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namhkse.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
