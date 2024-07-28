81575fe0e0dcf58b1ad6dfe49da02052debf132c
namhk <namhkse@gmail.com>
2024-07-29 06:52:45 +0700

    feat: Add swagger UI

--- src/test/java/com/namhkse/springboot_rest/SpringbootRestApplicationTests.java
+++ src/test/java/com/namhkse/springboot_rest/SpringbootRestApplicationTests.java
@@ -1,13 +1,4 @@
 package com.namhkse.springboot_rest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SpringbootRestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
